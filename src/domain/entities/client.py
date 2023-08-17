dfe3b1bc123a8276646a72fe3f3dca1122cfdcd2
Briqz23 <dbriquez.devmaua@gmail.com>
2023-08-16 23:25:43 -0300

    enums

--- src/domain/entities/client.py
+++ src/domain/entities/client.py
@@ -1,20 +1,41 @@
-class Client:
-    date : int
-    name: str
-    idade: int
-    esclaridade: str
-    depression_score: float
-    anxiety_score: float
-    stress_score: float
+from enum import Enum
 
-    def __init__(self, date: int, name: str, idade:int, escolaridade:str, depression_score: float,
-                  anxiety_score: float, stress_score: float):
+class Escolaridade(Enum):
+    FUNDAMENTAL_INCOMPLETO = "Ensino Fundamental incompleto"
+    FUNDAMENTAL_COMPLETO = "Ensino Fundamental completo"
+    MEDIO_INCOMPLETO = "Ensino Médio incompleto"
+    MEDIO_COMPLETO = "Ensino Médio completo"
+    SUPERIOR_INCOMPLETO = "Ensino Superior incompleto"
+    SUPERIOR_COMPLETO = "Ensino Superior completo"
+
+class Idade(Enum):
+    ATE_18 = "até 18"
+    ENTRE_19_30 = "19-30"
+    ENTRE_31_40 = "31-40"
+    ENTRE_41_50 = "41-50"
+    ENTRE_51_60 = "51-60"
+    ENTRE_61_70 = "61-70"
+    ACIMA_70 = "+70"
+
+class Client:
+
+    def __init__(self, cpf: str, escolaridade: Escolaridade, idade: Idade, date: int, name: str, 
+                 depression_score: float, anxiety_score: float, stress_score: float):
+        self.cpf = cpf
+        self.escolaridade = escolaridade
+        self.idade = idade
         self.date = date
         self.name = name
-        self.idade = idade
-        self.escolaridade = escolaridade
         self.depression_score = depression_score 
         self.anxiety_score = anxiety_score
         self.stress_score = stress_score
     
-cliente1 = Client(date=20230728, name="João da Silva", depression_score=3.5, anxiety_score=2.7, stress_score=4.1)
+    def __str__(self):
+        return f"CPF: {self.cpf}\nEscolaridade: {self.escolaridade.value}\nIdade: {self.idade.value}\nDate: {self.date}\nName: {self.name}\nDepression Score: {self.depression_score}\nAnxiety Score: {self.anxiety_score}\nStress Score: {self.stress_score}"
+
+# Exemplo de uso
+cliente1 = Client(cpf="12345678900", escolaridade=Escolaridade.MEDIO_COMPLETO, idade=Idade.ENTRE_19_30,
+                  date=20230728, name="João da Silva", depression_score=3.5, anxiety_score=2.7, stress_score=4.1)
+
+# Imprime a representação personalizada
+print(cliente1)
---
+++ src/domain/enums/sexualidade_enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Sexualidade(Enum):
+    CISGENERO = "Cisgenero"
+    TRANSGENERO = "Transgenero"
+    NAO_BINARIO = "Nao Binario"
+    
