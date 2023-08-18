4a0653d785686ca117656c449bf71d85adab5a77
Briqz23 <dbriquez.devmaua@gmail.com>
2023-08-18 10:26:15 -0300

    refat

--- src/domain/entities/client.py
+++ src/domain/entities/client.py
@@ -17,31 +17,58 @@ class AGE(Enum):
     ENTRE_61_70 = "61-70"
     ACIMA_70 = "+70"
 
-class GENDER(Enum):
-    CISGENERO = "Cisgênero"
-    TRANSGENERO = "Transgênero"
-    NAO_BINARIO = "Não Binário"
+class SEX(Enum):
+    MAN = "Homem"
+    WOMAN = "Mulher"
 
 class Client:
 
-    def __init__(self, cpf: str, education_level: EDUCATION_LEVEL, age: AGE, gender: GENDER, date: int, name: str, 
-                 depression_score: float, anxiety_score: float, stress_score: float, suggestion: str):
-        self.cpf = cpf
-        self.education_level = education_level
-        self.age = age
-        self.gender = gender
-        self.date = date
+    def __init__(self, name: str, cpf: str, age: AGE, education_level: EDUCATION_LEVEL, sex: SEX, date: int,
+                 state: str, job: str, role: str, depression_score: float, anxiety_score: float, stress_score: float,
+                 suggestion: str, mail: str):
         self.name = name
-        self.depression_score = depression_score 
+        self.cpf = cpf
+        self.age = age
+        self.education_level = education_level
+        self.sex = sex
+        self.date = date  #usar epoch 
+        self.state = state
+        self.job = job
+        self.role = role
+        self.depression_score = depression_score
         self.anxiety_score = anxiety_score
         self.stress_score = stress_score
         self.suggestion = suggestion
-    
+        self.mail = mail
+
     def __str__(self):
-        return f"CPF: {self.cpf}\nEducation level: {self.education_level.value}\nAge: {self.age.value}\nGender: {self.gender.value}\nDate: {self.date}\nName: {self.name}\nDepression Score: {self.depression_score}\nAnxiety Score: {self.anxiety_score}\nStress Score: {self.stress_score}\nSuggestion: {self.suggestion}"
+        return f"Client(name={self.name}, cpf={self.cpf}, age={self.age}, education_level={self.education_level}, " \
+               f"sex={self.sex}, date={self.date}, state={self.state}, job={self.job}, role={self.role}, " \
+               f"depression_score={self.depression_score}, anxiety_score={self.anxiety_score}, " \
+               f"stress_score={self.stress_score}, suggestion={self.suggestion}, mail={self.mail})"
 
-cliente1 = Client(cpf="12345678900", education_level=EDUCATION_LEVEL.MEDIO_COMPLETO, age=AGE.ENTRE_19_30,
-                  gender=GENDER.CISGENERO, date=20230728, name="João da Silva", depression_score=3.5, anxiety_score=2.7, stress_score=4.1,
-                  suggestion="Consider taking breaks and practicing mindfulness.")
+client1 = Client(name="John Doe", cpf="12345678900", age=AGE.ENTRE_19_30, education_level=EDUCATION_LEVEL.SUPERIOR_COMPLETO,
+                 sex=SEX.MAN, date=1990, state="California", job="Software Engineer", role="Developer",
+                 depression_score=3.5, anxiety_score=2.0, stress_score=4.2,
+                 suggestion="Practice mindfulness daily.", mail="john@example.com")
 
-print(cliente1)
+client2 = Client(name="Jane Smith", cpf="98765432100", age=AGE.ENTRE_31_40, education_level=EDUCATION_LEVEL.MEDIO_COMPLETO,
+                 sex=SEX.WOMAN, date=1985, state="New York", job="Marketing Manager", role="Manager",
+                 depression_score=2.0, anxiety_score=3.8, stress_score=3.5,
+                 suggestion="Try engaging in regular exercise.", mail="jane@example.com")
+
+client3 = Client(name="Michael Johnson", cpf="45678912300", age=AGE.ENTRE_41_50, education_level=EDUCATION_LEVEL.SUPERIOR_INCOMPLETO,
+                 sex=SEX.MAN, date=1972, state="Texas", job="Teacher", role="Educator",
+                 depression_score=1.5, anxiety_score=1.2, stress_score=2.8,
+                 suggestion="Explore a new hobby for relaxation.", mail="michael@example.com")
+
+client4 = Client(name="Emily Brown", cpf="78912345600", age=AGE.ENTRE_19_30, education_level=EDUCATION_LEVEL.MEDIO_INCOMPLETO,
+                 sex=SEX.WOMAN, date=1995, state="Florida", job="Student", role="College Student",
+                 depression_score=4.0, anxiety_score=4.5, stress_score=3.7,
+                 suggestion="Consider talking to a counselor.", mail="emily@example.com")
+
+# Printing the objects
+print(client1)
+print(client2)
+print(client3)
+print(client4)
